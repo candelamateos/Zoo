459b012a8b39b5ccacdcf814909e59d88fe1497f
R0b0tik0 <robotiko.maigat@gmx.com>
2025-02-13 19:19:16 +0100

    clase 5

--- Prueba.py
+++ Prueba.py
@@ -21,6 +21,7 @@ for i in cosas:
     nueva_lista.append(elemento)
 
 """""
+
 frase = "Hola esto es una prueba, probando 1 2 3"
 def contarletras(frase):
     contador = {}
@@ -29,6 +30,34 @@ def contarletras(frase):
         contador[letra] = contador.get(letra, 0) + 1
 
     print(contador)
+
+#contarletras(frase)
+
+
+k = 23
+
+numeros = [1,2,3,6,7,8,9,10,72, 15, 9, 13]
+
+def sumar_k(k, numeros):
+    coincidencias = 0
+    for i in range(len(numeros)):
+        for j in range(i+1, len(numeros)):
+            if numeros[i] + numeros[j] == k:
+                coincidencias += 1
+    return coincidencias
+
+#print(sumar_k(k, numeros))
+
+numeros_dic = {}
+
+def sumar_k_diccionario(k, numeros):
+    coincidencias = 0
+    for numero in numeros:
+        numeros_dic[numero] = k - numero
     
-contarletras(frase)
-    
+    for clave in numeros_dic:
+        if numeros_dic[clave] in numeros_dic:
+            coincidencias += 1
+    return coincidencias//2
+        
+print(sumar_k_diccionario(k, numeros))
